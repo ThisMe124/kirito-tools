f0c19337ec1a9715f0bdc06b1b6bbb443a68eaed
Strong Heart <88891071+ThisMe124@users.noreply.github.com>
2022-01-04 18:58:32 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch')
+const baseurl = "http://kirito-db-api.vercel.app/"
+const chalk = require("chalk")
+
+async function test() {
+    console.log("Hello Banh") 
+}
+
+
+module.exports = {
+    test
+}
