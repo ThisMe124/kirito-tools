b0c0e7648e197e8b045bbcd97d2c9ba54d2b10a6
Strong Heart <88891071+ThisMe124@users.noreply.github.com>
2022-01-04 19:03:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ async function test() {
     console.log("Hello Banh") 
 }
 
+async function status(package) {
+    const crs = await fetch(`${baseurl}/db/status?code=${encodeURIComponent(code)}`)
+    const result = await crs.json()
+    if (!code) throw new Error("Error: No package name provided")
+    if(!r) throw new Error("Fetch Error")
+    return result.createdAt
+}
 
 module.exports = {
     test
