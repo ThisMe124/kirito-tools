ec88b2518dad413ab77782458188a3e5ded88592
Strong Heart <88891071+ThisMe124@users.noreply.github.com>
2022-05-08 10:36:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,30 +8,6 @@ async function test() {
     console.log("Hello Banh") 
 }
 
-async function status(code) {
-    if (!code) throw new Error("Error: No code provided")
-    const crs = await fetchJson(`http://kirito-db-api.vercel.app/db/status?code=${encodeURIComponent(code)}`)
-    //const result = await crs.json()
-    if(!crs) throw new Error("Fetch Error Or Code Invalid")
-    return crs.createdAt
-}
-
-async function testDB() {
-    const crs = await fetchJson(`http://cdn.kiritosenpai.xyz/test/get`)
-    if(!crs) throw new Error("Fetch Error")
-    return crs.tes
-}
-
-async function whatsappWebVersion() {
-    const data = await phin({
-      url: `${baseURL}/check-update?version=1&platform=web`,
-      method: "get",
-      parse: "json",
-    });
-    if(!data) throw new Error("Fetch Error")
-    return data.body.currentVersion
-}
-
 async function whatsappWebVersion() {
   const nice = fetch(`${baseURL}/check-update?version=1&platform=web`)
   const text = (await (await nice).json()).currentVersion
@@ -39,35 +15,20 @@ async function whatsappWebVersion() {
 }
 
 async function whatsappWebVersionFull() {
-    const crs = await fetch(`${baseURL}/check-update?version=1&platform=web`)
-    const res = await crs.json()
+    const res = await require("node-fetch")(`${baseURL}/check-update?version=1&platform=web`).then((v) => v.json());
     if(!res) throw new Error("Fetch Error")
     return res
 }
 
 async function loli() {
-const gf = await require("node-fetch")("https://raw-kirito.is-a.dev/nsfw-loli.json").then((v) => v.json());
-const res = {}
-res.url = gf[Math.floor(Math.random() * gf.length)]
-return res
+    const gf = await require("node-fetch")("https://raw-kirito.is-a.dev/nsfw-loli.json").then((v) => v.json());
+    const res = {}
+    res.url = gf[Math.floor(Math.random() * gf.length)]
+    return res
 }
-
-async function get(endpoint, params) {
-        let fetchURL = `${baseURL}/${endpoint}`;
-        if (params) {
-            fetchURL += stringify(params);
-        }
-        const res = await fetch(fetchURL);
-        if (res.isBelowSoft !== true) throw res;
-        const data = await res.json();
-        return data;
-    }
 
 module.exports = {
     loli,
-    test, 
-    status,
-    testDB,
     whatsappWebVersion, 
     whatsappWebVersionFull
 }
