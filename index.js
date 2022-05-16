d988f587376e1afa6c15a0f99937adf4fde4075d
Strong Heart <88891071+ThisMe124@users.noreply.github.com>
2022-05-16 21:32:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,18 @@ async function fetchJson(url, options) {
      }
 }
 
-
+async function parseTime(seconds) {
+      seconds = Number(seconds)
+      var d = Math.floor(seconds / (3600 * 24));
+      var h = Math.floor(seconds % (3600 * 24) / 3600);
+      var m = Math.floor(seconds % 3600 / 60);
+      var s = Math.floor(seconds % 60);
+      var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+      var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+      var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+      var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+      return dDisplay + hDisplay + mDisplay + sDisplay;
+}
 
 async function translate(text = null, l = "id") {
     result = {};
@@ -93,6 +104,7 @@ module.exports = {
     loli,
     sleep,
     isUrl, 
+    parseTime,
     translate,
     getRandom,
     getBuffer,
