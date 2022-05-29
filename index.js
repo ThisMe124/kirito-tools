9017b203830ea40bb2cc8ecdc4d6e3fcaba88bf3
Strong Heart <88891071+ThisMe124@users.noreply.github.com>
2022-05-29 19:41:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require('axios')
 const FormData = require("form-data")
 const got = require('got')
 const cheerio = require('cheerio')
+const pdfkit = require('pdfkit')
 
 async function test() {
     console.log("Hello Banh") 
@@ -180,6 +181,36 @@ headers: {
 })
 }
 
+async function getFile(url) {
+const res = await got({
+url: url,
+method: 'get',
+responseType: 'buffer'
+})
+return res.rawBody
+}
+
+async function toPdf(images = [], size = 'A4') {
+return new Promise(async(resolve, reject) => {
+if (!Array.isArray(images)) throw new TypeError('images must be an array');
+let buffs = [];
+const doc = new pdfkit({ margin: 0, size: size });
+for (let img of images) {
+const data = await getFile(img)
+doc.image(data, 0, 0, {
+fit: [595.28, 841.89],
+align: 'center',
+valign: 'center',
+})
+doc.addPage();
+}
+doc.on('data', (chunk) => buffs.push(chunk));
+doc.on('end', () => resolve(Buffer.concat(buffs)));
+doc.on('error', (err) => reject(err));
+doc.end()
+})
+}
+
 function getNhentaiInfo(id) {
 	return new Promise(async(resolve, reject) => {
 		const req = await got('https://nhentai.net/g/' + id + '/')
@@ -232,6 +263,8 @@ module.exports = {
     jsonString,
     getNhentaiInfo, 
     translateText, 
+    toPdf, 
+    getFile,
     whatsappWebVersion, 
     whatsappWebVersionFull
 }
